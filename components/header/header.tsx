b7ddc2754ebda04c9c129a1dee79c3545f532557
Alexander Voievodin <mendelberg44@gmail.com>
2022-09-19 23:25:01 +0200

    init

---
+++ components/header/header.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import styles from './header.module.scss';
+
+export default class Header extends Component {
+  render() {
+    return <div className={styles.header}>Header</div>;
+  }
+}
